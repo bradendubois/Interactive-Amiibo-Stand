de68e633750bf9e61f7ee4fc4a61a41c598c4522
Braden <drab3n@gmail.com>
2020-05-13 19:32:26 -0600

    Moved old sketches to separate directory, created file for CIDs, code rework
    Continuing to revise the newest version of the sketch, created a directory to put all the old ones in for preservation's sake, and starting a new file(s) to make a sort of table showing the CID of everything. The tables showing CIDs will likely be structured in a way that others can contribute to.

---
+++ NEW_README.md
@@ -0,0 +1,154 @@
+
+
+// Captain Falcon == 6
+
+// Samus == 49157
+
+ //Little Mac = 49158)      
+            
+
+          //Fox == 32773)      
+            
+         //Wii Fit == 7)      
+            
+          //Villager == 32769)      
+            
+          //Marth == 33)      
+    
+          //Yoshi == 768)      
+    
+          //Pit == 16391)      
+    
+          //Kirby == 31)      
+    
+          //Pikachu == 6425)      
+    
+          //Link == 1)      
+    
+          //Luigi == 256)      
+    
+          //Diddy == 2304)      
+    
+          //DK == 2048)      
+    
+          //Peach == 512)      
+    
+          //Zelda == 257)      
+    
+          //Mario == 0)      
+    
+          //Toonlink == 65537)      
+    
+          //Dedede == 543)      
+    
+          //Lucario == 49178)      
+    
+          //Bowser == 1280)      
+    
+          //Mega Man == 32820)      
+    
+          //Sonic == 50)      
+    
+          //Rosalina == 66560)      
+    
+          //MetaKnight == 287)      
+    
+          //Shulk == 16418)      
+    
+          //Sheik == 65793)      
+    
+          //Ike == 289)      
+    
+          //Robin == 801)      
+    
+          //Lucina == 545)      
+    
+          //Charizard == 1561)      
+    
+          //Wario == 1792)      
+    
+          //Pac-Man == 16435)      
+    
+          //Ness == 32802)      
+    
+          //Gannondorf == 66049)      
+    
+          //Greninja == 37403)      
+    
+          //Jigglypuff == 10009)      
+    
+          //Dark Pit == 16647)      
+    
+          //Palutena == 16903)      
+    
+          //Zero Suit == 114693)      
+    
+          //Dr Mario == 65536)      
+          
+          //Bowser Jr == 1536)      
+    
+          //Olimar Song == 81926)      
+    
+          //Mr Game & Watch == 32775)      
+    
+          //Rob == 33031)      
+    
+          //Duck Hunt == 33287)      
+    
+          //Mii Brawler == 49159)      
+    
+          //Mii Swordfighter == 114695)      
+    
+          //Mii Gunner == 180231)      
+    
+          //Mewtwo == 38425)      
+    
+          //Falco == 33029)      
+    
+          //Lucas == 33058)      
+    
+          //Shovel Knight == 49205)      
+    
+          //Roy == 1234)      
+    
+          //Ryu == 1234)      
+    
+          //Cloud == 1234)      
+    
+          //Corrin == 1234)      
+    
+          //Bayonetta == 1234)      
+    
+          //Inkling Girl == 65544)      
+    
+          //Inkling Boy == 131080)      
+    
+          //Squid == 196616)      
+    
+          //Chibi Robo == 49186)      
+    
+          //Yarn Yoshi == 33620736)      
+    
+          //Isabelle == 98561)      
+    
+          //Tom Nook == 33537)      
+    
+          //KK Song == 33281)      
+    
+          //Mabel == 34817)      
+    
+          //Digby == 35841)      
+    
+          //Lottie == 49409)      
+    
+          //Cyrus Song == 35585)      
+    
+          //Reese == 35329)      
+    
+          //Blathers == 37377)      
+    
+          //Celeste == 37633)      
+    
+          //Resetti == 36353)      
+    
+          //Kicks == 37889)     
--- amiibo_stand/amiibo_stand.ino
+++ amiibo_stand/amiibo_stand.ino
@@ -33,10 +33,10 @@ uint32_t versiondata;
 uint32_t lastcard = 0;
 uint32_t currentcard = 1;
 uint32_t CID = 0;
-boolean  introPlaying = false;
+boolean introStarted = false;
 
 //Loop()
-uint32_t cardidentifier = 0;
+uint32_t cardidentifier;
 uint8_t success;
 uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; // Buffer to store the returned UID
 uint8_t uidLength; // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
@@ -44,25 +44,20 @@ uint8_t uidLength; // Length of the UID (4 or 7 bytes depending on ISO14443A car
 //////////////////////////////////// SETUP
 
 void setup() {
+  
   // set up Serial library at 115200 bps
   Serial.begin(115200);
     
   PgmPrintln("Amiibo Scanner");
   
-  if (!card.init()) {
-    error("Card init. failed!");
-  }
-  if (!vol.init(card)) {
-    error("No partition!");
-  }
-  if (!root.openRoot(vol)) {
-    error("Couldn't open dir");
-  }
+  if (!card.init()) error("Card init. failed!");
+  if (!vol.init(card)) error("No partition!");
+  if (!root.openRoot(vol)) error("Couldn't open dir");
   
   PgmPrintln("Files found:");
   root.ls();
   
-  // find Adafruit RFID/NFC shield
+  // Find Adafruit RFID/NFC shield
   nfc.begin();
 
   versiondata = nfc.getFirmwareVersion();
@@ -70,6 +65,7 @@ void setup() {
     Serial.print(F("Didn't find PN53x board"));
     while (1); // halt
   }
+  
   // Got ok data, print it out!
   Serial.print(F("Found chip PN5")); Serial.println((versiondata>>24) & 0xFF, HEX);
   Serial.print(F("Firmware ver. ")); Serial.print((versiondata>>16) & 0xFF, DEC);
@@ -84,8 +80,6 @@ void setup() {
 }
 
 /////////////////////////////////// LOOP
-
-unsigned digit = 0;
 
 void loop() {
   
@@ -103,11 +97,10 @@ void loop() {
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
   cardidentifier = 0;
-  CID = 999;  // This probably doesn't need to be here?
-  if (success) 
-  {
-    // Found a card!
 
+  // Read a card
+  if (success) {
+    
     Serial.print(F("Amiibo detected #"));
     // turn the four byte UID of a mifare classic into a single variable #
     cardidentifier = uid[3];
@@ -116,10 +109,9 @@ void loop() {
     cardidentifier <<= 8; cardidentifier |= uid[0];
     Serial.println(cardidentifier);
     
-    //Check the previous card
-    
-    (lastcard = currentcard);
-    (currentcard = cardidentifier);
+    // Compare to the previous card
+    lastcard = currentcard;
+    currentcard = cardidentifier;
     
     Serial.print(F("Last Amiibo:    #"));
     Serial.println(lastcard);
@@ -127,13 +119,15 @@ void loop() {
     Serial.println(currentcard);
     
     // Check Character ID
-    
-      // Try to read the Character info page (#21)
-      uint8_t charID[32];
-      success = nfc.mifareultralight_ReadPage (21, charID);
-      
+  
+    // Try to read the Character info page (#21)
+    uint8_t charID[32];
+    success = nfc.mifareultralight_ReadPage (21, charID);
+
+    // Successfully read the Character info page
     if (success) {
-        // turn page 21 into a character ID
+      
+      // turn page 21 into a character ID
       CID = charID[6];
       CID <<= 8; CID |= charID[6];
       CID <<= 8; CID |= charID[5];
@@ -142,6 +136,7 @@ void loop() {
       CID <<= 8; CID |= charID[2];
       CID <<= 8; CID |= charID[1];
       CID <<= 8; CID |= charID[0];
+      
       Serial.println("Character Number: ");
       Serial.println(CID);
       
@@ -149,8 +144,8 @@ void loop() {
       if (currentcard == lastcard) {
 
 
-        // Haven't started the into *yet*; start it
-        if (!introPlaying) {
+        // Haven't started the Intro *yet*; start it
+        if (!introStarted) {
 
           // Convert the CID to the intro filename  
           char* introFileName = intro_ify(CID);
@@ -160,219 +155,35 @@ void loop() {
 
           // Mark that the first song for this CID is now playing
           if (wave.isplaying) {
-             introPlaying = true;
+             introStarted = true;
           }
-            
-          if(!wave.isplaying) 
+        
+        } else if (!wave.isplaying) {
+          // Intro Complete; Play the main song
+
+          //Captain Falcon
+          if (CID == 6) 
           {
-            introPlaying = false;
+            strcpy_P(buffer, (char*)pgm_read_word(&(string_table[0])));
+            playcomplete(buffer);
           }
-
-          //Captain Falcon Song
-          if (CID == 6)    
-          
-          //Samus Song
-          if (CID == 49157)      
-          {strcpy_P(buffer, (char*)pgm_read_word(&(string_table[31])));
-            playfile(buffer);
-            if (wave.isplaying) {
-               introPlaying = true;
-            }
-          }
-          
-          
-          //Little Mac = 49158)      
-            
-          //Fox == 32773)      
-            
-         //Wii Fit == 7)      
-            
-          //Villager == 32769)      
-            
-          //Marth == 33)      
-
-          //Yoshi == 768)      
-
-          //Pit == 16391)      
-
-          //Kirby == 31)      
-
-          //Pikachu == 6425)      
-
-          //Link == 1)      
-
-          //Luigi == 256)      
-
-          //Diddy == 2304)      
-
-          //DK == 2048)      
-
-          //Peach == 512)      
-
-          //Zelda == 257)      
-
-          //Mario == 0)      
-
-          //Toonlink == 65537)      
-
-          //Dedede == 543)      
-
-          //Lucario == 49178)      
-
-          //Bowser == 1280)      
-
-          //Mega Man == 32820)      
-
-          //Sonic == 50)      
-
-          //Rosalina == 66560)      
-
-          //MetaKnight == 287)      
-
-          //Shulk == 16418)      
-
-          //Sheik == 65793)      
-
-          //Ike == 289)      
-
-          //Robin == 801)      
-
-          //Lucina == 545)      
-
-          //Charizard == 1561)      
-
-          //Wario == 1792)      
-
-          //Pac-Man == 16435)      
-
-          //Ness == 32802)      
-
-          //Gannondorf == 66049)      
-
-          //Greninja == 37403)      
-
-          //Jigglypuff == 10009)      
-
-          //Dark Pit == 16647)      
-
-          //Palutena == 16903)      
-
-          //Zero Suit == 114693)      
-
-          //Dr Mario == 65536)      
-          
-          //Bowser Jr == 1536)      
-
-          //Olimar Song == 81926)      
-
-          //Mr Game & Watch == 32775)      
-
-          //Rob == 33031)      
-
-          //Duck Hunt == 33287)      
-
-          //Mii Brawler == 49159)      
-
-          //Mii Swordfighter == 114695)      
-
-          //Mii Gunner == 180231)      
-
-          //Mewtwo == 38425)      
-
-          //Falco == 33029)      
-
-          //Lucas == 33058)      
-
-          //Shovel Knight == 49205)      
-
-          //Roy == 1234)      
-
-          //Ryu == 1234)      
-
-          //Cloud == 1234)      
-
-          //Corrin == 1234)      
-
-          //Bayonetta == 1234)      
-
-          //Inkling Girl == 65544)      
-
-          //Inkling Boy == 131080)      
-
-          //Squid == 196616)      
-
-          //Chibi Robo == 49186)      
-
-          //Yarn Yoshi == 33620736)      
-
-          //Isabelle == 98561)      
-
-          //Tom Nook == 33537)      
-
-          //KK Song == 33281)      
-
-          //Mabel == 34817)      
-
-          //Digby == 35841)      
-
-          //Lottie == 49409)      
-
-          //Cyrus Song == 35585)      
-
-          //Reese == 35329)      
-
-          //Blathers == 37377)      
-
-          //Celeste == 37633)      
-
-          //Resetti == 36353)      
-
-          //Kicks == 37889)      
-
-        
-          
-      }
-      /*
-        There is no song playing, play the intro sequence!
-      */
-      else 
-      {
-        //Captain Falcon
-        if (CID == 6) 
-        {
-          strcpy_P(buffer, (char*)pgm_read_word(&(string_table[0])));
-          playcomplete(buffer);
         }
-        //Samus
-        if (CID == 49157) 
-        {strcpy_P(buffer, (char*)pgm_read_word(&(string_table[1])));
-          playcomplete(buffer);
-        }
+      } else {
         
+        // Clear/reset all data
+        wave.stop();
+        lastcard = 999999999;
+        currentcard = 999999998;
+        introStarted = false;
       }
-    }
-    else 
-    {
-
+    } else {
+      // Amiibo removed; Clear/reset all data
       wave.stop();
       lastcard = 999999999;
       currentcard = 999999998;
-       introPlaying = false;
+      introStarted = false;
     }
-  } 
-
-  //If the Amiibo is gone, stop the song.
-  else 
-    {
-      //if (currentcard == lastcard) 
-      //{
-        //Serial.println("UID else");
-        wave.stop();
-      //}  
-      lastcard = 999999999;
-      currentcard = 999999998;
-       introPlaying = false;
-    }  
+  }
 }
 
 /////////////////////////////////// HELPERS
